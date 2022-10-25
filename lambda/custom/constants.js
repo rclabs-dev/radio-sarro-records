0fc5773c3f11034dfa1740412d02f14afb561301
rcosta <regivaldocosta@yahoo.com.br>
2022-10-25 18:54:47 -0300

    Create constants.js

---
+++ lambda/custom/constants.js
@@ -0,0 +1,17 @@
+/* CONSTANTS */
+
+exports.skill = {
+  appId: '',
+  dynamoDBTableName: 'Radio-Sarro-Multi-Stream',
+};
+
+exports.audioData = [
+  {
+    title: 'Episódio 1',
+    url: 'https://listen.radioking.com/radio/467555/stream/523739',
+  },
+  {
+    title: 'Episode 2',
+    url: 'https://listen.radioking.com/radio/488324/stream/546263',
+  },
+];
