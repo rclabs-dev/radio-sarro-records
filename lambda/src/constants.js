5a54309f5bf601f7eecf48381e4b69ab2f0bb54e
rcosta <regivaldocosta@yahoo.com.br>
2022-10-25 18:32:31 -0300

    Update constants.js

--- lambda/src/constants.js
+++ lambda/src/constants.js
@@ -7,11 +7,11 @@ exports.skill = {
 
 exports.audioData = [
   {
-    title: 'Episode 139',
-    url: 'https://feeds.soundcloud.com/stream/274166909-amazon-web-services-306355661-aws-podcast-episode-139.mp3',
+    title: 'Episódio 1',
+    url: 'https://listen.radioking.com/radio/467555/stream/523739',
   },
   {
-    title: 'Episode 140',
-    url: 'https://feeds.soundcloud.com/stream/275202399-amazon-web-services-306355661-amazon-web-services.mp3',
+    title: 'Episode 2',
+    url: 'https://listen.radioking.com/radio/488324/stream/546263',
   },
 ];
